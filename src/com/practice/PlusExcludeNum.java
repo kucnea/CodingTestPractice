bfe78747623324e12f8209ae0e414a0e588a570a
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-09-23 23:23:45 +0900

    2/ test1

--- src/com/practice/PlusExcludeNum.java
+++ src/com/practice/PlusExcludeNum.java
@@ -3,23 +3,38 @@ package com.practice;
 public class PlusExcludeNum {
 
 	public static void main(String[] args) {
-		int[] numbers = null;
+		
+		
+		
+		int[] numbers = {1,2,3,4,6,7,8,0};
 		int result = solution(numbers);
-		System.out.println(result);
+		System.out.println("result : "+result);
 		
 	}
 
 	private static int solution(int[] numbers) {
 		int answer = 0;
-		int cnt =0;
+		int cnt =1;
+		
 		for (int i = 0; i < numbers.length; i++) {
+			
+			if(cnt==10) break;
+			
 			if(cnt==numbers[i]) {
+				cnt++;
+				i=-1;
+			}else {
+				
+				if(i==numbers.length-1) {
+					answer+=cnt;
+					cnt++;
+					i=-1;
+				}
 				
 			}
+			
 		}
 		
-		
-//		
 		return answer;
 	}
 
