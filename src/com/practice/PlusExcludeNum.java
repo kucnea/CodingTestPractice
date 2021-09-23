5e13d4b38a1ff3ee291921b5a84c60e4978d7e33
kucnea@naver.com <81126604+kucnea@users.noreply.github.com>
2021-09-23 22:55:44 +0900

    1/ codingTestPractice setting

---
+++ src/com/practice/PlusExcludeNum.java
@@ -0,0 +1,26 @@
+package com.practice;
+
+public class PlusExcludeNum {
+
+	public static void main(String[] args) {
+		int[] numbers = null;
+		int result = solution(numbers);
+		System.out.println(result);
+		
+	}
+
+	private static int solution(int[] numbers) {
+		int answer = 0;
+		int cnt =0;
+		for (int i = 0; i < numbers.length; i++) {
+			if(cnt==numbers[i]) {
+				
+			}
+		}
+		
+		
+		
+		return answer;
+	}
+
+}
