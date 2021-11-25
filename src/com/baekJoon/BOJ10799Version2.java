1e7e7706ea5b55d3ab80cdc6cab5e5efde7ef69f
kucnea <kucnea@naver.com>
2021-11-25 22:21:47 +0900

    43 / How many count computer during 1sec.

---
+++ src/com/baekJoon/BOJ10799Version2.java
@@ -0,0 +1,37 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class BOJ10799Version2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String q = br.readLine();
+		int answer = solution(q);
+//		System.out.println("answer : "+answer);
+		bw.write(answer);
+		bw.flush();
+		bw.close();
+	}
+
+	private static int solution(String q) {
+		
+		int count = 0;
+		
+		for (int i = 0; i < q.length(); i++) {
+			
+			
+			
+		}
+		
+		return count;
+	}
+
+}
