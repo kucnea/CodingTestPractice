86f5cfe7cdda2b0d2b23a4526b3fe0ffe5ff45df
kucnea <kucnea@naver.com>
2021-12-04 13:21:21 +0900

    57 / Making Heap Test 1

--- src/com/algorithm/heapTest/HeapTest.java
+++ src/com/algorithm/heapTest/HeapTest.java
@@ -1,7 +1,55 @@
 package com.algorithm.heapTest;
 
-public class HeapTest {
+import java.util.Comparator;
+
+public class HeapTest<E> {
 	
+	private final Comparator<? super E> comparator;
+	private static final int DefaultCapacity = 10; // 최소(기본) 용적 크기
+	
+	private int size; // 요소 개수
+	
+	private Object[] array; // 요소를 담을 배열
+	
+	//생성자 Type 1 ( 초기 공간 할당 X )
+	public HeapTest() {
+		this(null);
+	}
+	
+	public HeapTest(Comparator<? super E> comparator) {
+		this.array = new Object[DefaultCapacity];
+		this.size = 0;
+		this.comparator = comparator;
+	}
+	
+	
+	// 생성자 Type 2 ( 초기 공간 할당 O )
+	public HeapTest(int capacity) {
+		this(capacity, null);
+	}
+
+	public HeapTest(int capacity, Comparator<? super E> comparator) {
+		this.array = new Object[capacity];
+		this.size = 0;
+		this.comparator = comparator;
+	}
+	
+	
+	
+	// 받은 인덱스의 부모 노드 인덱스를 반환
+	private int getParent(int index) {
+		return index / 2;
+	}
+	
+	// 받은 인덱스의 왼쪽 자식 노드 인덱스를 반환
+	private int getLeftChild(int index) {
+		return index * 2;
+	}
+	
+	// 받은 인덱스의 오른쪽 자식 노드 인덱스를 반환
+	private int getRightChild(int index) {
+		return getLeftChild(index) + 1;      // 물론, index*2 + 1 을 해도 된다.
+	}
 	
 	
 }
--- src/com/javaStudy/GenericTest/GenericTest.java
+++ src/com/javaStudy/GenericTest/GenericTest.java
@@ -1,32 +1,32 @@
 package com.javaStudy.GenericTest;
 
 public class GenericTest {
-	
-	public static class Person<K,V>{
+
+	public static class Person<K, V> {
 		private K key;
 		private V value;
-		
+
 		void set(K key, V value) {
 			this.key = key;
 			this.value = value;
 		}
-		
+
 		String get() {
-			return key+", "+value;
+			return key + ", " + value;
 		}
-		
-		Person(){ }
+
+		Person() {
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		
-		Person<String,Integer> p = new Person<>(); 
+
+		Person<String, Integer> p = new Person<>();
 		p.set("이름", 10);
-		
+
 		System.out.println(p.get());
-		System.out.println("key Type : "+p.key.getClass().getName());
-		System.out.println("value Type : "+p.value.getClass().getName());
+		System.out.println("key Type : " + p.key.getClass().getName());
+		System.out.println("value Type : " + p.value.getClass().getName());
 	}
-	
-	
+
 }
