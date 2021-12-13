826775fcc8f82773cff4223e45f007b419bb901d
kucnea <kucnea@naver.com>
2021-12-13 09:41:52 +0900

    62 / Recursive Fuction Test

---
+++ src/com/algorithm/recursiveFunction/RecursiveFunction.java
@@ -0,0 +1,16 @@
+package com.algorithm.recursiveFunction;
+
+public class RecursiveFunction {
+
+	public static void main(String[] args) {
+		
+		RF();
+
+	}
+	
+	static void RF() {
+		System.out.println("Recursive Function called");
+		RF();
+	}
+	
+}
