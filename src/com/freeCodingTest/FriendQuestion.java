1e23ef6f27f47b513dd7598bd1e45f26626b0ac3
kucnea <kucnea@naver.com>
2021-12-14 20:45:35 +0900

    67 / add remark

--- src/com/freeCodingTest/FriendQuestion.java
+++ src/com/freeCodingTest/FriendQuestion.java
@@ -28,21 +28,30 @@ public class FriendQuestion {
 
 	private static String solution(String str) {
 		
+		// str을 변형해서 활용할 것이라 원본 저장
 		String result = str;
 		
+		// 앞,뒤 공백 혹시 모르니 제거
 		str = str.trim();
+		// 빈칸 제거
 		str = str.replaceAll(" ", "");
+		// Hypen(-) 제거
 		str = str.replaceAll("-", "");
 		
+		// 숫자외에 있을 시 개인정보 아닙니다 반환
 		if(!str.matches("[0-9]+")) return "["+result+"] 는 개인정보가 아닙니다.";
 		
 //		System.out.println("str : "+str);
 		
+		// 길이 값 사용하기 편하게, 근데 많이 안쓰네
 		int length = str.length();
+		// 주민번호 검증에 사용할 변수
 		int temp = 0;
 		
+		// 공백과 Hypen을 제거하면 주민번호는 총 13자리
 		if(length==13) {
 			for (int i = 0; i < length-1; i++) {
+				// 주민번호 검증을 위한 공식
 				if(i<=7) temp+=(Integer.parseInt(str.substring(i, i+1))*(i+2));
 				else temp+=(Integer.parseInt(str.substring(i, i+1))*(i-6));
 //				System.out.println("i : "+i+", str.substring(i,i+1) : "+str.substring(i,i+1)+", temp : "+temp);
@@ -51,10 +60,11 @@ public class FriendQuestion {
 			if(Integer.parseInt(str.substring(12, 13))==temp) return "["+result+"] 는 주민번호가 맞습니다.";
 		}
 		
+		// 공백과 Hypen을 제거하면 카드번호는 총 16자리
 		if(length==16) return "["+result+"] 는 카드번호가 맞습니다."; 
 			
 		
-		
+		// 어디에도 해당하지 않으면 개인정보 아닙니다 반환
 		return "["+result+"] 는 개인정보가 아닙니다.";
 	}
 
