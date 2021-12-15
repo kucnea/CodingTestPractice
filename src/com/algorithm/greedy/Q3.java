007600cd3e0d2afe159e67d96f72961c405ade6f
kucnea <kucnea@naver.com>
2021-12-16 02:38:28 +0900

    74 / Greedy algorithm matter 3

---
+++ src/com/algorithm/greedy/Q3.java
@@ -0,0 +1,32 @@
+package com.algorithm.greedy;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		
+		String s = "02984";
+		int result = solution(s);
+		System.out.println("result : "+result);
+		
+	}
+
+	private static int solution(String s) {
+		
+		int count = Integer.parseInt(s.substring(0,1));
+		
+		for (int i = 1; i < s.length(); i++) {
+			
+			int temp = Integer.parseInt(s.substring(i, i+1));
+			
+			if(count==0) count+=temp;
+			else {
+				if(temp==0) count+=temp;
+				else count*=temp;	
+			}
+			
+		}
+		
+		return count;
+	}
+
+}
