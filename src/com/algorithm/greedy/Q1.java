7134561104d27dd1ed7faca4b057b7da01275641
kucnea <kucnea@naver.com>
2021-12-16 02:12:02 +0900

    72 / Greedy algorithm matter 1 upgrade

--- src/com/algorithm/greedy/Q1.java
+++ src/com/algorithm/greedy/Q1.java
@@ -6,8 +6,23 @@ public class Q1 {
 		
 		int money = 1300; // 단, money는 항상 10의 배수
 		int count = solution(money);
+		int count2 = solution2(money);
 		System.out.println("동전의 최소 개수 : "+count);
+		System.out.println("동전의 최소 개수2 : "+count2);
 		
+	}
+
+	private static int solution2(int money) {
+		
+		int[] coin = {500, 100, 50, 10};
+		int count = 0;
+		
+		for (int i = 0; i < coin.length; i++) {
+			count += money/coin[i];
+			money = money%coin[i];
+		}
+		
+		return count;
 	}
 
 	private static int solution(int money) {
