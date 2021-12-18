dea3fd9e22e9623e1b41fc28decd5ec60242e587
kucnea <kucnea@naver.com>
2021-12-19 04:32:49 +0900

    86 / InsertSort Test less count

--- src/com/algorithm/sortTest/InsertSort.java
+++ src/com/algorithm/sortTest/InsertSort.java
@@ -6,10 +6,14 @@ public class InsertSort {
 		
 		int[] list = {3,4,5,1,23,5,6,3,1,4};
 		list = solution(list);
+		int[] list2 = solution(list);
 		for(int i = 0; i < list.length; i++) {
 			System.out.print(list[i]+" ");
 		}
-		
+		System.out.println();
+		for(int i=0 ; i<list.length; i++) {
+			System.out.print(list2[i]+" ");
+		}
 	}
 	
 	private static int[] solution(int[] list) {
@@ -26,6 +30,22 @@ public class InsertSort {
 		}
 		
 		return list; 
+	}
+	
+	private static int[] solution2(int[] list) {
+		
+		for(int i=1 ; i<list.length;i++) {
+			int temp = 0;
+			for(int j=i; j>=0 ; j--) {
+				if(list[j]>list[i]) {
+					temp = list[j];
+					list[j] = list[i];
+					list[i] = temp;
+				}
+			}
+		}
+		
+		return list;
 	}
 
 }
