da334b4db6cdd0bdf43756627aa6d7fa067a6c2d
kucnea <kucnea@naver.com>
2021-12-19 04:16:17 +0900

    85 / InsertSort Test

---
+++ src/com/algorithm/sortTest/InsertSort.java
@@ -0,0 +1,31 @@
+package com.algorithm.sortTest;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		
+		int[] list = {3,4,5,1,23,5,6,3,1,4};
+		list = solution(list);
+		for(int i = 0; i < list.length; i++) {
+			System.out.print(list[i]+" ");
+		}
+		
+	}
+	
+	private static int[] solution(int[] list) {
+		
+		for(int i=1; i<list.length;i++) {
+			int temp = 0;
+			for(int j = 0; j < i; j++ ) {
+				if(list[j]>list[i]) {
+					temp = list[j];
+					list[j] = list[i];
+					list[i] = temp;
+				}
+			}
+		}
+		
+		return list; 
+	}
+
+}
