6aa4161e4cac20dd503e01462fa56bbe71a25e5e
kucnea <kucnea@naver.com>
2021-12-20 04:51:31 +0900

    92 / BinarySearch, Direct Search Test arrange version

--- src/com/algorithm/binarySearch/ParametricSearch.java
+++ src/com/algorithm/binarySearch/ParametricSearch.java
@@ -96,8 +96,30 @@ public class ParametricSearch {
 			
 		}
 		
-		
 		return h;
+	}
+	
+	private static int solution3(int[] list, int m) {
+		int result = 0 ;
+		int start = 0 ;
+		int end = (int) 1e9;
+		
+		while(start<=end) {
+			
+			long count = 0;
+			int mid = (start + end) / 2;
+			for(int i = 0 ; i<list.length ; i++) {
+				if(list[i]>mid) count += list[i] - mid;
+			}
+			if(count < m) end = mid-1;
+			else {
+				result = mid;
+				start = mid+1;
+			}
+			
+		}
+		
+		return result;
 	}
 	
 	
