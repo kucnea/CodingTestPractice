1e726db4668a7caf0b50eb2170bb61bf3bef6d51
kucnea <kucnea@naver.com>
2021-12-20 23:10:53 +0900

    94 / Binary Search Q1

--- src/com/algorithm/binarySearch/Q1.java
+++ src/com/algorithm/binarySearch/Q1.java
@@ -1,16 +1,24 @@
 package com.algorithm.binarySearch;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Q1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		Scanner sc = new Scanner(System.in);
+		String s = br.readLine();
 		
-		int x = sc.nextInt();
+		int x = Integer.parseInt(s);
 		
-		String s = sc.next();
+		s = br.readLine();
+		System.out.println(s);
 		String[] ss = s.split(" ");
 		int[] list = new int[ss.length];
 		
@@ -18,66 +26,148 @@ public class Q1 {
 			list[i] = Integer.parseInt(ss[i]);
 		}
 		
-		long startTime = System.currentTimeMillis();
-		int result = solution(list,x);
-		long endTime = System.currentTimeMillis();
-		long time = (endTime - startTime)/1000;
+//		long startTime = System.currentTimeMillis();
+//		int result = solution(list,x);
+//		long endTime = System.currentTimeMillis();
+//		long time = (endTime - startTime)/1000;
 		
-		System.out.println("result : "+result);
-		System.out.println("time : "+time);
+		long startTime = System.currentTimeMillis();
+		int result2 = right(list,x)-left(list,x)+1;
+		long endTime = System.currentTimeMillis();
+		long time2 = (endTime-startTime)/1000;
+		
+		
+//		System.out.println("result : "+result);
+//		System.out.println("time : "+time);
+		System.out.println("result2 : "+result2);
+		System.out.println("time2 : "+time2);
 		
 	}
 
-	private static int solution(int[] list, int x) {
-		int left = 0;
-		int right = 0;
-		int start = 0;
+//	private static int solution(int[] list, int x) {
+//		int left = 0;
+//		int right = 0;
+//		int start = 0;
+//		int end = list.length-1;
+//		
+//		int leftTemp = 0;
+//		int rightTemp = 0;
+//		while(start<=end) {
+//			int mid = (start+end)/2;
+//			
+//			if(list[mid]>x) end = mid-1;
+//			if(list[mid]<x) start = mid+1;
+//		
+//			if(list[mid]==x) {
+//				left = (start+mid)/2;
+//				if(list[start]<x) {
+//					if(list[left]<x) start = left+1;
+//					else {
+//						leftTemp = start+1;
+//						start = (start+left)/2;
+//					}
+//				}else {
+//					if(list[leftTemp]!=x) {
+//						if(list[(leftTemp+start)/2]==x) start = ((leftTemp+start)/2);
+//						else leftTemp = ((leftTemp+start)/2)+1;
+//					}
+//					else left = leftTemp;
+//				}
+//				
+//				right = (mid+end)/2;
+//				if(list[end]>x) {
+//					if(list[right]>x) end = right-1;
+//					else {
+//						rightTemp = end-1;
+//						end = (end+right)/2;
+//					}
+//				}else {
+//					if(list[rightTemp]!=x) {
+//						if(list[(rightTemp+end)/2]==x) end = ((rightTemp+end)/2);
+//						else rightTemp = ((rightTemp+end)/2)-1;
+//					}else {
+//						right = rightTemp;
+//					}
+//				}
+//			}
+//		}
+//		
+//		int result = right-left+1;
+//		return result;
+//	}
+
+	
+	
+	private static int left(int[] list, int x) {
+		int start = 0 ;
 		int end = list.length-1;
 		
-		int leftTemp = 0;
-		int rightTemp = 0;
-		while(start<=end) {
+		while(start<end) {
+			
 			int mid = (start+end)/2;
 			
 			if(list[mid]>x) end = mid-1;
 			if(list[mid]<x) start = mid+1;
-		
+			
 			if(list[mid]==x) {
-				left = (start+mid)/2;
 				if(list[start]<x) {
-					if(list[left]<x) start = left+1;
-					else {
-						leftTemp = start+1;
-						start = (start+left)/2;
-					}
+					end = mid;
+					start += 1;
 				}else {
-					if(list[leftTemp]!=x) {
-						if(list[(leftTemp+start)/2]==x) start = ((leftTemp+start)/2);
-						else leftTemp = ((leftTemp+start)/2)+1;
-					}
-					else left = leftTemp;
-				}
-				
-				right = (mid+end)/2;
-				if(list[end]>x) {
-					if(list[right]>x) end = right-1;
-					else {
-						rightTemp = end-1;
-						end = (end+right)/2;
-					}
-				}else {
-					if(list[rightTemp]!=x) {
-						if(list[(rightTemp+end)/2]==x) end = ((rightTemp+end)/2);
-						else rightTemp = ((rightTemp+end)/2)-1;
-					}else {
-						right = rightTemp;
-					}
+					end = mid-1;
 				}
 			}
+			
 		}
 		
-		int result = right-left+1;
-		return result;
+		return start;
 	}
-
+	
+	private static int right(int[] list, int x) {
+		int start = 0 ;
+		int end = list.length-1;
+		
+		while(end>start) {
+			
+			System.out.println("start : "+start+"\nend : "+end);
+			
+			int mid = (start+end)/2;
+			
+			if(list[mid]>x) end = mid-1;
+			if(list[mid]<x) start = mid+1;
+			
+			if(list[mid]==x) {
+				if(list[end]>x) {
+					start = mid;
+					end += -1;
+				}else {
+					start = mid+1;
+				}
+			}
+			
+		}
+		
+		return end;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
