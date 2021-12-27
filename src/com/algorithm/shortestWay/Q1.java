ee5483be9e797b397b6936809537e693c46651d2
kucnea <kucnea@naver.com>
2021-12-27 13:48:38 +0900

    110 / ShortestWay Q1 implementing

---
+++ src/com/algorithm/shortestWay/Q1.java
@@ -0,0 +1,103 @@
+package com.algorithm.shortestWay;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q1 {
+
+	static final int inf = (int)1e9;
+	static int n;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		String[] ss = s.split(" ");
+		
+		n = Integer.parseInt(ss[0]); // 도시의 개수
+		int m = Integer.parseInt(ss[1]); // 통로의 개수
+		int c = Integer.parseInt(ss[2]); // 메세지를 보내고자 하는 도시
+		
+		int[][] map = new int[n+1][n+1];
+		
+		for(int i = 0 ; i < m ; i++) {
+			s = br.readLine();
+			ss = s.split(" ");
+			
+			int start = Integer.parseInt(ss[0]);
+			int end = Integer.parseInt(ss[1]);
+			int dis = Integer.parseInt(ss[2]);
+			
+			map[start][end] = dis;
+		}
+		
+		long startTime = System.currentTimeMillis();
+		String result = solution(map,c);
+		long endTime = System.currentTimeMillis();
+		long time = (endTime - startTime)/1000;
+		long memory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		
+		bw.write("result : "+result);
+		bw.write("\ntime : "+time);
+		bw.write("\nmemory : "+(memory/1048576));
+		
+		bw.flush();
+		br.close();
+		bw.close();
+		
+	}
+	
+	
+	
+	private static String solution(int[] map, int c) {
+		String result = "";
+		
+		
+		
+		
+		return result;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
