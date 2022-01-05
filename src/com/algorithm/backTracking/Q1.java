1c0321c8c114f1cec8a008432ce2f6e8f76979c2
kucnea <kucnea@naver.com>
2022-01-06 03:36:37 +0900

    133 / BackTracking Test1

---
+++ src/com/algorithm/backTracking/Q1.java
@@ -0,0 +1,58 @@
+package com.algorithm.backTracking;
+
+import java.io.*;
+
+public class Q1 {
+	
+	private static int[] list;
+	private static boolean[] check;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		String[] ss = s.split(" ");
+		int n = Integer.parseInt(ss[0]);
+		int m = Integer.parseInt(ss[1]);
+		
+		list = new int[n+1];
+		check = new boolean[n+1];
+		for(int i = 1 ; i < list.length ; i++) {
+			list[i] = i;
+		}
+		
+		String[] result = solution(n,m);
+		
+		for(int i = 0 ; i < result.length ; i++) {
+			System.out.println(result[i]);
+		}
+		
+	}
+
+	private static String[] solution(int n, int m) {
+		
+		int count = 1;
+		for(int i = 0 ; i < m ; i++) {
+			count*=(n-i);
+		}
+		String[] result = new String[count/2];		
+		String temp = "";
+		count = 0;
+		
+		for(int i = 1 ; i < list.length; i++) {
+			System.out.println("i : "+i);
+			for(int j = i+1 ; j < list.length ; j++) {
+				temp = list[i]+"";
+				temp += list[j];
+				result[count] = temp;
+				count++;
+			}
+			
+		}
+		
+		return result;
+	}
+	
+	
+}
