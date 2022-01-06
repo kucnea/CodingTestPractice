6074f5f3d6384068c45d3a092f60e9a3818d9d71
kucnea <kucnea@naver.com>
2022-01-07 00:38:47 +0900

    137 / BOJ10171

---
+++ src/com/baekJoon/BOJ10171.java
@@ -0,0 +1,16 @@
+package com.baekJoon;
+
+public class BOJ10171 {
+
+	public static void main(String[] args) {
+
+		System.out.println(
+		"\\    /\\"+"\n"+
+		" )  ( ')"+"\n"+
+		"(  /  )"+"\n"+
+		" \\(__)|"+"\n"
+			);
+		
+	}
+
+}
