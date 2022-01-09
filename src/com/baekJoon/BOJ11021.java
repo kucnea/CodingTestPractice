d186cea686eafc81e0183ccd652a510754bccb5d
kucnea <kucnea@naver.com>
2022-01-10 00:48:39 +0900

    154 / BOJ11021

---
+++ src/com/baekJoon/BOJ11021.java
@@ -0,0 +1,33 @@
+package com.baekJoon;
+
+import java.io.*;
+
+public class BOJ11021 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		String[] ss = s.split(" ");
+		
+		int t = Integer.parseInt(s);
+		
+		for(int i = 1 ; i <= t ; i++) {
+			s = br.readLine();
+			ss = s.split(" ");
+			
+			int a = Integer.parseInt(ss[0]);
+			int b = Integer.parseInt(ss[1]);
+			
+			String result = "Case #"+i+": "+(a+b);
+			bw.write(result+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+	
+}
