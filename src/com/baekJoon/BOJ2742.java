8879a9e16b0d6db8e9db8c90f085000ee10fbd0e
kucnea <kucnea@naver.com>
2022-01-09 19:53:49 +0900

    153 / BOJ2742

---
+++ src/com/baekJoon/BOJ2742.java
@@ -0,0 +1,27 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ2742 {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		int n = Integer.parseInt(s);
+		
+		for(int i = n ; i >= 1 ; i--) {
+			bw.write(i+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+}
