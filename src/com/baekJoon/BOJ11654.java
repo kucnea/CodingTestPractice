88d379430924bb367757e1269bc5faa7284a7d09
kucnea <kucnea@naver.com>
2022-01-22 16:36:00 +0900

    180 / BOJ11654

---
+++ src/com/baekJoon/BOJ11654.java
@@ -0,0 +1,17 @@
+package com.baekJoon;
+
+import java.io.*;
+
+public class BOJ11654 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char c = br.readLine().charAt(0);
+		int result = (int) c;
+		System.out.println(result);
+		
+	}
+	
+}
