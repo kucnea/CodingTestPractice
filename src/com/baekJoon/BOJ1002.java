3d1bee84a4adbe243bfb05595eb673ebf5d29bbd
kucnea@naver.com <kucnea@naver.com>
2022-02-18 16:31:23 +0900

    212 / BOJ1002 Ing

---
+++ src/com/baekJoon/BOJ1002.java
@@ -0,0 +1,21 @@
+package com.baekJoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1002 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		int t = Integer.parseInt(s);
+		for(int i = 0 ; i < t ; i++) {
+			
+		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	}
+}
