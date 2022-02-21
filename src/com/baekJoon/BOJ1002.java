4294fa091ddc68739a8c2cbccca85bf11314cee8
kucnea <kucnea@naver.com>
2022-02-21 15:57:46 +0900

    214 / BOJ1002

--- src/com/baekJoon/BOJ1002.java
+++ src/com/baekJoon/BOJ1002.java
@@ -13,9 +13,37 @@ public class BOJ1002 {
 		String s = br.readLine();
 		int t = Integer.parseInt(s);
 		for(int i = 0 ; i < t ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());	
+			int ax = Integer.parseInt(st.nextToken());
+			int ay = Integer.parseInt(st.nextToken());
+			int ar = Integer.parseInt(st.nextToken());
+			
+			int cx = Integer.parseInt(st.nextToken());
+			int cy = Integer.parseInt(st.nextToken());
+			int cr = Integer.parseInt(st.nextToken());
+			
+			int dx = Math.abs(ax-cx);
+			int dy = Math.abs(ay-cy);
+			double dis = Math.sqrt((Math.pow(dx, 2)+Math.pow(dy, 2)));
+			
+			int temp = 0;
+			if((ax==cx) && (ay==cy) && (ar==cr)) temp = -1;
+			else {
+				if((ar+cr)<dis) temp = 0;
+				else if((ar+cr)==dis) temp = 1;
+				else if((ar==cr+dis) || (cr==ar+dis)) temp = 1;
+				else if((ar>dis && (dis+cr)<ar) || (cr>dis && (dis+ar)< cr)) temp = 0;
+				else if((ar+cr)>dis) temp = 2;	
+			}
+			
+			
+			String result = temp+"\n";
+			bw.write(result);
 			
 		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw.flush();
+		bw.close();
+		br.close();
 		
 	}
 }
