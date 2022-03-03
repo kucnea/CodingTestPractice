8addf83efefa6abc79c7fd50b05f18acfc967fce
kucnea <kucnea@naver.com>
2022-03-04 00:07:19 +0900

    222 / BOJ1436

--- src/com/baekJoon/BOJ1436.java
+++ src/com/baekJoon/BOJ1436.java
@@ -8,32 +8,32 @@ import java.io.OutputStreamWriter;
 public class BOJ1436 {
 
 	public static void main(String[] args) throws Exception {
-		
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	        
-	        String s = br.readLine();
-	        int n = Integer.parseInt(s);
-	        int count = 1;
-	        int start = 666;
-	        while(true){
-	            if(n==1) break;
-	            start++;
-	            String temp = start+"";
-	            if(temp.contains("666")){
-	                count++;
-	                if(n==count) break;
-	            }
-	            
-	            
-	        }
-	        String t = start+"";
-	        bw.write(t);
-	        bw.flush();
-	        bw.close();
-	        br.close();
 
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String s = br.readLine();
+		int n = Integer.parseInt(s);
+		int count = 1;
+		int start = 666;
+		while (true) {
+			if (n == 1)
+				break;
+			start++;
+			String temp = start + "";
+			if (temp.contains("666")) {
+				count++;
+				if (n == count)
+					break;
+			}
+
+		}
+		String t = start + "";
+		bw.write(t);
+		bw.flush();
+		bw.close();
+		br.close();
+
 	}
-	
+
 }
