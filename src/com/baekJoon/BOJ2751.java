c4796d8d23edd07b53db313f4bad71b29d77e9fe
kucnea <kucnea@naver.com>
2022-03-05 22:30:34 +0900

    224 / BOJ2751 ing

---
+++ src/com/baekJoon/BOJ2751.java
@@ -0,0 +1,74 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ2751 {
+	
+	static int[] list;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		int n = Integer.parseInt(s);
+		list = new int[n];
+		
+		for(int i = 0 ; i < n ; i++) {
+			
+			s = br.readLine();
+			list[i] = Integer.parseInt(s);
+			
+		}
+		
+		
+		sort(0,1,list.length-1);
+		
+		for(int i = 0 ; i < n ; i++) {
+			String temp = list[i]+"\n";
+			bw.write(temp);
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+	
+	
+	private static void sort(int pivot, int start, int end) {
+		
+		
+		while(start<end) {
+			
+			int temp = 0;
+			int right = end;
+			int left = start;
+			boolean flag1 = false;
+			boolean flag2 = false;
+			
+			while(list[left]<list[pivot]) {
+				left++;
+				if(left>=list.length) flag1 = true;
+			}
+			while(list[right]>list[pivot]) {
+				right--;
+				if(right<=0) flag2 = true;
+			}
+			
+			if(flag1 && flag2) return;
+			
+			temp = list[left];
+			list[left] = list[right];
+			list[right] = temp;
+			
+		}
+		
+	}
+	
+	
+	
+}
